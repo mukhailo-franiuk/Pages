2c600dc356860ab2799e96a422cb16173aed0838
mukhailo-franiuk <vipersmf@gmail.com>
2023-04-10 21:14:33 +0300

    Added my work

---
+++ main.js
@@ -0,0 +1,5 @@
+let ckeck = true;
+document.querySelector('.header__mobile-menu').addEventListener('click',() => {
+    document.querySelector('.main__navigation').classList.toggle('is-open');
+    
+})
